ab49b30809ae11b47df7d3bd2e044a5240080e9e
roach <tmdgusya@naver.com>
2021-12-20 22:35:33 +0900

    init

---
+++ src/main/java/hello/typeconvertor/TypeConvertorApplication.java
@@ -0,0 +1,13 @@
+package hello.typeconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/typeconvertor/TypeConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.typeconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
