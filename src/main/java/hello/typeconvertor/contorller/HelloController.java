f0a1609d04c8235d5fe42935ea37b835ebea87e5
roach <tmdgusya@naver.com>
2021-12-20 22:44:41 +0900

    Controller Method 추가

---
+++ src/main/java/hello/typeconvertor/contorller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.typeconvertor.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer integer = Integer.valueOf(data); // 숫자 Type 으로 Convert
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+}
